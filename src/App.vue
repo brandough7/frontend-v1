ac3f2d4ebd503b7ecce80d79331584058ced830c
Brandough <jbryan.parot@gmail.com>
2020-08-27 13:34:10 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <b-navbar toggleable="lg">
+    <b-navbar-brand >NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+  </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
